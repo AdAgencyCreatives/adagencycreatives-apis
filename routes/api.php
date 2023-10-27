b334e15ee9135be33f37f234e7b9b13e53e0458b
Hamza <6793siddique@gmail.com>
2023-10-27 22:39:33 +0500

    Added applied jobs endpoint

--- routes/api.php
+++ routes/api.php
@@ -84,6 +84,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('links', LinkController::class, ['except' => ['index']]);
     Route::apiResource('attachments', AttachmentController::class, ['except' => ['index']]);
 
+    Route::get('applied_jobs', [ApplicationController::class, 'applied_jobs']);
     Route::apiResource('applications', ApplicationController::class); //->middleware('check.permissions:application');
     Route::apiResource('resumes', ResumeController::class)->middleware('check.permissions:resume');
     Route::apiResource('educations', EducationController::class, ['except' => ['index']])->middleware('check.permissions:education');
