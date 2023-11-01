b09b2e61347d20dd65f6b0744795b0d3c59aa178
Hamza <6793siddique@gmail.com>
2023-11-02 03:02:03 +0500

    Added All comments in

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -37,7 +37,7 @@ class Post extends Model
 
     public function comments()
     {
-        return $this->hasMany(Comment::class)->whereNull('parent_id');
+        return $this->hasMany(Comment::class)->whereNull('parent_id')->latest();
     }
 
     public function firstThreeComments()
