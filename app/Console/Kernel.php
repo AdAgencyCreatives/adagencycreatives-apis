034873ec475bedbdf2f30aeaa850120aaa086a48
Hamza <6793siddique@gmail.com>
2023-11-11 06:28:30 +0500

    Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('telescope:prune --hours=72')->daily();
+        $schedule->command('email:unread-message-count')->everyMinute();
     }
 
     /**
