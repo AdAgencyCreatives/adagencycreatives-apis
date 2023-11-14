9b638aed6767e19420c0745498584d9498ad13d1
Hamza <6793siddique@gmail.com>
2023-11-14 07:03:52 +0500

    Email line height

--- resources/views/emails/account/new_user_registration_agency.blade.php
+++ resources/views/emails/account/new_user_registration_agency.blade.php
@@ -5,13 +5,13 @@
     <td>
         <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
             <tr>
-                <td style="padding: 0 0 30px; font-family: sans-serif; mso-height-rule: exactly; line-height: 14px; color: #000000; font-size: 14px; position: relative;"
+                <td style="padding: 0 0 30px; font-family: sans-serif; mso-height-rule: exactly; color: #000000; font-size: 14px; position: relative;"
                     class="body_text_color body_text_size">
                     <h1
                         style="background: #fff; text-align: center; padding: 30px; border-bottom: 2px solid #000;     text-transform: uppercase;">
                         Registration request</h1>
                     <div
-                        style="background:#fff; border-radius: 5px; width: 450px; margin: 0 auto; color:#000000; line-height:1.5 !important">
+                        style="background:#fff; border-radius: 5px; width: 450px; margin: 0 auto; color:#000000; line-height:1.5 !important;">
                         <span style="font-weight: normal; font-size: 14px;" class="welcome">Hello Member Support,</span>
 
                         <p>The following user has requested to join <a href="{{ config('app.frontend_url') }}"
