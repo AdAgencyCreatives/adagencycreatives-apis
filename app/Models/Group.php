045e87084232b899543574335856db834a1c7c5d
Hamza <6793siddique@gmail.com>
2023-11-19 04:51:44 +0500

    Add member_id filters to groups endpoint

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -65,8 +65,14 @@ class Group extends Model
     public function scopeUserId(Builder $query, $user_id)
     {
         $user = User::where('uuid', $user_id)->firstOrFail();
-
         return $query->where('user_id', $user->id);
+    }
+
+    public function scopeMemberId(Builder $query, $user_id)
+    {
+        $user_id = User::where('uuid', $user_id)->pluck('id');
+        $group_id = GroupMember::where('user_id', $user_id)->pluck('group_id');
+        return $query->whereIn('id', $group_id);
     }
 
     public function getStatusAttribute($value)
