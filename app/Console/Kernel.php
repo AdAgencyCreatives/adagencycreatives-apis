aa690e70b882723e549705e907de89bd164a80b9
Hamza <6793siddique@gmail.com>
2023-11-30 05:11:07 +0500

    Check Missing locations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune --hours=272')->daily();
+        $schedule->command('telescope:prune --hours=720')->daily();
         $schedule->command('email:unread-message-count')->daily('10:00');
     }
 
