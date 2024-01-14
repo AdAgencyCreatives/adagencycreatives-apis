6b8c3f1b9551d48480a535e4034b14ae9e6ebcf7
Hamza <6793siddique@gmail.com>
2024-01-14 07:12:12 +0500

    BUg Fixed

--- app/Http/Controllers/Admin/PackageRequestController.php
+++ app/Http/Controllers/Admin/PackageRequestController.php
@@ -31,10 +31,8 @@ class PackageRequestController extends Controller
     {
         try {
             $package_request = PackageRequest::where('uuid', $uuid)->firstOrFail();
-            $agency = Agency::find($package_request->user_id);
+            $agency = Agency::where('user_id', $package_request->user_id);
 
-            dump($package_request);
-            dump($agency);
             if ($request->input('assigned_to') !== '-1') {
                 $advisor_id = $request->input('assigned_to');
                 $advisor = User::find($advisor_id);
