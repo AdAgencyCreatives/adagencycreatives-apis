66612c855cc6482d9992828d4b2af67d6b516e87
Hamza <6793siddique@gmail.com>
2024-02-03 03:09:13 +0500

    Fix

--- app/Http/Resources/Job/JobLoggedInResource.php
+++ app/Http/Resources/Job/JobLoggedInResource.php
@@ -67,7 +67,7 @@ class JobLoggedInResource extends JsonResource
             $data['agency']['logo'] = getAttachmentBasePath() . $sub_agency_logo?->path;
 
             $data['agency']['logo_id'] = $this->attachment_id ? $sub_agency_logo?->uuid : null;
-            $data['agency']['fallback_image'] = $user->agency_logo;
+            $data['agency']['fallback_image'] = get_profile_picture($user);
 
         }
 
