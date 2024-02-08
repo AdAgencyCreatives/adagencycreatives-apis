71d51dbd6c43c5f877a373646637fd13a63b8852
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-02-08 21:44:35 +0500

    sort desc

--- app/Http/Controllers/Api/V1/ApplicationController.php
+++ app/Http/Controllers/Api/V1/ApplicationController.php
@@ -30,7 +30,9 @@ class ApplicationController extends Controller
                 AllowedFilter::scope('user_id'),
                 AllowedFilter::scope('job_id'),
                 'status',
-            ]);
+            ])
+            ->defaultSort('-created_at')
+            ->allowedSorts('created_at');;
 
         $query = $query->with('job');
         $applications = $query->paginate($request->per_page ?? config('global.request.pagination_limit'));
