7d15899f018943ff60f0e4b4bb43f70fa1655d12
Muzammil Ur Rehman Upwork <muzammil-rehman-upwork@gmail.com>
2024-03-05 03:41:10 +0500

    edited_at

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -16,7 +16,8 @@ class Message extends Model
         'message',
         'type',
         'created_at',
-        'updated_at'
+        'updated_at',
+        'edited_at'
     ];
 
     public function sender()
