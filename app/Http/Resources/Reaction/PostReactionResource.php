1a66e40683cf67e085b2d948816521e66796fb16
Muzammil Ur Rehman Upwork <muzammil-rehman-upwork@gmail.com>
2024-03-09 01:13:02 +0500

    post reaction slug fix

--- app/Http/Resources/Reaction/PostReactionResource.php
+++ app/Http/Resources/Reaction/PostReactionResource.php
@@ -16,6 +16,7 @@ class PostReactionResource extends JsonResource
             'user_id' => $user->uuid,
             'post_id' => $this->post->uuid,
             'user' => $user->full_name,
+            'username' => $user->username,
             'profile_picture' => get_profile_picture($user),
             'reaction_type' => $this->type,
             'created_at' => $this->created_at->format(config('global.datetime_format')),
