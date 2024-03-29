fb09f8ac334eda6ad81038e76de56e68fe6a8e09
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-03-30 01:50:40 +0500

    fix

--- app/Http/Resources/CreativeSpotlight/CreativeSpotlightResource.php
+++ app/Http/Resources/CreativeSpotlight/CreativeSpotlightResource.php
@@ -15,11 +15,10 @@ class CreativeSpotlightResource extends JsonResource
             'id' => $this->uuid,
             'title' => $this->title,
             'slug' => $this->slug,
-            'url' => getAttachmentBasePath().$this->path,
+            'url' => getAttachmentBasePath() . $this->path,
             'status' => $this->status,
             'created_at' => $carbonDate->format('M d, Y'),
-            'updated_at' => $this->updated_at->format(config('global.datetime_format')),
+            'updated_at' => $this?->updated_at ? $this->updated_at->format(config('global.datetime_format')) : $carbonDate->format('M d, Y'),
         ];
-
     }
 }
