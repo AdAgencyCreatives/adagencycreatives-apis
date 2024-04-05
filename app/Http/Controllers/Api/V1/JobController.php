cd36a08cab8cab86ce6cb2845f33157aaa7eac12
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-04-06 03:53:56 +0500

    fix

--- app/Http/Controllers/Api/V1/JobController.php
+++ app/Http/Controllers/Api/V1/JobController.php
@@ -45,7 +45,7 @@ class JobController extends Controller
                 AllowedFilter::scope('city_id'),
                 AllowedFilter::scope('state_slug'),
                 AllowedFilter::scope('city_slug'),
-                AllowedFilter::scope('agency'),
+                // AllowedFilter::scope('agency'),
                 'title',
                 'slug',
                 'employment_type',
