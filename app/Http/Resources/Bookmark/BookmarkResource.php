1df86a4f74d5f56afc62ad4bebf4f2fb08cc6da5
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-04-13 01:54:56 +0500

    fix

--- app/Http/Resources/Bookmark/BookmarkResource.php
+++ app/Http/Resources/Bookmark/BookmarkResource.php
@@ -47,12 +47,12 @@ class BookmarkResource extends JsonResource
 
         switch ($this->bookmarkable_type) {
             case 'App\Models\Creative':
-                $model = Creative::where('id', $this->bookmarkable_id)->firstOrFail();
+                $model = Creative::where('user_id', $this->bookmarkable_id)->firstOrFail();
                 if ($model->user) {
                     return new CreativeResource($model);
                 }
             case 'App\Models\Agency':
-                $model = Agency::where('id', $this->bookmarkable_id)->firstOrFail();
+                $model = Agency::where('user_id', $this->bookmarkable_id)->firstOrFail();
                 if ($model->user) {
                     return new AgencyResource($model);
                 }
