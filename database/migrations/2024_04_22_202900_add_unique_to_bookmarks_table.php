c7e625f2909f2416fb32b1cdf9379205d21a0929
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-04-23 01:29:54 +0500

    add unique constraint to bookmarks

---
+++ database/migrations/2024_04_22_202900_add_unique_to_bookmarks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            //
+            $table->unique(['user_id', 'bookmarkable_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            //
+        });
+    }
+};
