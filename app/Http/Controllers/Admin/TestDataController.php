e8c2e84452bd734ac7b08287532f078f1e5629d8
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-07 23:51:34 +0100

    test

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -79,28 +79,54 @@ class TestDataController extends Controller
     //     return view('pages.test_data.index', ['data' => $data]);
     // }
 
+    // public function index(Request $request)
+    // {
+    //     $job = json_decode(json_encode(array('category_id' => $request?->cid ?? 0)), FALSE);
+
+    //     $categories = [];
+
+    //     $category = Category::where('id', $job->category_id)->first();
+    //     // return view('pages.test_data.index', ['data' => $category]);
+
+    //     $group_categories = Category::where('group_name', $category->name)->get();
+
+    //     if (count($group_categories) > 0) {
+    //         for ($i = 0; $i < count($group_categories); $i++) {
+    //             $categories[$i] = $group_categories[$i]->id;
+    //         }
+    //     } else {
+    //         $categories[0] = $category->id;
+    //     }
+
+    //     // return view('pages.test_data.index', ['data' => $categories]);
+
+    //     $data = JobAlert::with('user')->whereIn('category_id', $categories)->where('status', 1)->get();
+    //     return view('pages.test_data.index', ['data' => $data]);
+    // }
+
     public function index(Request $request)
     {
-        $job = json_decode(json_encode(array('category_id' => $request?->cid ?? 0)), FALSE);
+        $receiver = json_decode(json_encode(array(
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+        )), FALSE);
 
-        $categories = [];
+        $sender = json_decode(json_encode(array(
+            'first_name' => 'Creative',
+            'last_name' => 'Two',
+            'profile_url' => 'creative-two',
+        )), FALSE);
 
-        $category = Category::where('id', $job->category_id)->first();
-        // return view('pages.test_data.index', ['data' => $category]);
+        $profile_url = $sender->profile_url;
 
-        $group_categories = Category::where('group_name', $category->name)->get();
-
-        if (count($group_categories) > 0) {
-            for ($i = 0; $i < count($group_categories); $i++) {
-                $categories[$i] = $group_categories[$i]->id;
-            }
-        } else {
-            $categories[0] = $category->id;
-        }
-
-        // return view('pages.test_data.index', ['data' => $categories]);
-
-        $data = JobAlert::with('user')->whereIn('category_id', $categories)->where('status', 1)->get();
-        return view('pages.test_data.index', ['data' => $data]);
+        $data = [
+            'receiver' => $receiver,
+            'data' => [
+                'recipient' => $receiver->first_name,
+                'inviter' => $sender->first_name,
+                'iniviter_profile' => sprintf("%s%s", env('FRONTEND_URL'), $profile_url),
+            ],
+        ];
+        return view('emails.friendship.request', ['data' => $data]);
     }
 }
