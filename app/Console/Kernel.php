24e2793222f61503a1a79035f301cca5dc4854b9
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-08 13:14:18 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:unread-mention-notification')->dailyAt($daily_time);
         $schedule->command('adagencycreatives:schedule-notifications')->everyFifteenMinutes();
         // $schedule->command('portfolio_latest:generate')->everyMinute();
-        $schedule->command('email:send-friend-request-emails')->dailyAt($daily_time);
+        if (env('APP_ENV') != 'production') {
+            $schedule->command('email:send-friend-request-emails')->dailyAt($daily_time);
+        }
     }
 
     /**
