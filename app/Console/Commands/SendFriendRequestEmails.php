be091b7af6deb6936f50411be2d814f665cd7225
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-10 20:02:44 +0100

    bundle senders / inviters

--- app/Console/Commands/SendFriendRequestEmails.php
+++ app/Console/Commands/SendFriendRequestEmails.php
@@ -52,27 +52,35 @@ class SendFriendRequestEmails extends Command
 
         $friendRequests = FriendRequest::where('status', 'pending')
             ->whereDate('updated_at', '=', $date_range)
-            ->with('sender', 'receiver')
+            ->orderBy('receiver_id')->orderByDesc('updated_at')
             ->get();
+
+        $bundle = [];
+        $receivers = [];
 
         foreach ($friendRequests as $fr) {
             $receiver = $fr->receiver;
             $sender = $fr->sender;
 
-            if ($sender->role == 'creative') {
-                $profile_url = '/creative/' . $sender->creative?->slug ?? '';
-            } elseif ($sender->role == 'agency') {
-                $profile_url = '/agency/' . $sender->agency?->slug ?? '';
+            $sender->profile_picture = get_profile_picture($sender);
+
+            if (array_key_exists($receiver->id, $bundle)) {
+                $bundle[$receiver->id][count($bundle)] = $sender;
             } else {
-                $profile_url = $sender->username;
+                $bundle[$receiver->id] = array(0 => $sender);
+                $receivers[count($receivers)] = $receiver;
             }
+        }
+
+        foreach ($receivers as $receiver) {
+            $senders = $bundle[$receiver->id];
 
             SendEmailJob::dispatch([
                 'receiver' => $receiver,
                 'data' => [
                     'recipient' => $receiver->first_name,
-                    'inviter' => $sender->first_name,
-                    'iniviter_profile' => sprintf("%s%s", env('FRONTEND_URL'), $profile_url),
+                    'senders' => $senders,
+                    'multiple' => count($senders) > 0,
                 ],
             ], 'friendship_request_sent');
         }
