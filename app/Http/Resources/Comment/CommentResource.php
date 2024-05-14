f12907347af4abc51e2d1b8c29d4a6e68d9a3d05
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-14 16:45:35 +0100

    fix

--- app/Http/Resources/Comment/CommentResource.php
+++ app/Http/Resources/Comment/CommentResource.php
@@ -20,6 +20,7 @@ class CommentResource extends JsonResource
             'user' => $user->first_name.' '.$user->last_name,
             'user_slug' => get_user_slug($user),
             'profile_picture' => get_profile_picture($user),
+            'user_thumbnail' => get_user_thumbnail($user),
             'post_id' => $this->post->uuid,
             'parent_id' => isset($this->parent) ? $this->parent->uuid : null,
             'content' => $this->content,
