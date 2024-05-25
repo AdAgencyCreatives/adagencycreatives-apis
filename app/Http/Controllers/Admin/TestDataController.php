1a6c7265939d1c061ef7820405ef6846e63c753c
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-25 04:22:54 +0100

    fix

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -250,11 +250,11 @@ class TestDataController extends Controller
         $yesterday = now()->subDay()->toDateString();
         $today = now()->toDateString();
         $jobs = Job::where('apply_type', 'Internal')->where(function ($query) use ($yesterday, $today) {
-           $query->where(function ($q) use ($yesterday, $today) {
+            $query->where(function ($q) use ($yesterday, $today) {
                 $q->where('status', 4)->whereDate('updated_at', '>=', $yesterday)->where('updated_at', '<', $today);
             })->orWhere(function ($q) use ($yesterday, $today) {
                 $q->whereDate('expired_at', '>=', $yesterday)->where('expired_at', '<', $today);
-            })
+            });
         })->get();
 
         return view('pages.test_data.index', ['data' => $jobs]);
