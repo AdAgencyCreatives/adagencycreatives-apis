c7270ab7b40b929d12952efa117b3322ee88a800
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-29 16:35:33 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('portfolio_latest:generate')->everyMinute();
 
         $schedule->command('email:send-friend-request-emails')->dailyAt($daily_time);
+
         $schedule->command('email:send-job-closed-emails')->dailyAt($daily_time);
     }
 
