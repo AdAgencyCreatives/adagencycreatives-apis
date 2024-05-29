2bd1f510c03bde1ad9d691e80e8c7d85ce7731a8
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-05-29 16:41:36 +0100

    fix

--- app/Console/Commands/SendFriendRequestEmails.php
+++ app/Console/Commands/SendFriendRequestEmails.php
@@ -49,40 +49,41 @@ class SendFriendRequestEmails extends Command
     public function handle()
     {
         $date_range = now()->subDay()->format('Y-m-d');
+        $this->info('Fine now?');
 
-        $friendRequests = FriendRequest::where('status', 'pending')
-            ->whereDate('updated_at', '=', $date_range)
-            ->orderBy('receiver_id')->orderByDesc('updated_at')
-            ->get();
+        // $friendRequests = FriendRequest::where('status', 'pending')
+        //     ->whereDate('updated_at', '=', $date_range)
+        //     ->orderBy('receiver_id')->orderByDesc('updated_at')
+        //     ->get();
 
-        $bundle = [];
-        $receivers = [];
+        // $bundle = [];
+        // $receivers = [];
 
-        foreach ($friendRequests as $fr) {
-            $receiver = $fr->receiver;
-            $sender = $fr->sender;
+        // foreach ($friendRequests as $fr) {
+        //     $receiver = $fr->receiver;
+        //     $sender = $fr->sender;
 
-            $sender->profile_picture = get_profile_picture($sender);
+        //     $sender->profile_picture = get_profile_picture($sender);
 
-            if (array_key_exists($receiver->id, $bundle)) {
-                $bundle[$receiver->id][count($bundle)] = $sender;
-            } else {
-                $bundle[$receiver->id] = array(0 => $sender);
-                $receivers[count($receivers)] = $receiver;
-            }
-        }
+        //     if (array_key_exists($receiver->id, $bundle)) {
+        //         $bundle[$receiver->id][count($bundle)] = $sender;
+        //     } else {
+        //         $bundle[$receiver->id] = array(0 => $sender);
+        //         $receivers[count($receivers)] = $receiver;
+        //     }
+        // }
 
-        foreach ($receivers as $receiver) {
-            $senders = $bundle[$receiver->id];
+        // foreach ($receivers as $receiver) {
+        //     $senders = $bundle[$receiver->id];
 
-            SendEmailJob::dispatch([
-                'receiver' => $receiver,
-                'data' => [
-                    'recipient' => $receiver->first_name,
-                    'senders' => $senders,
-                    'multiple' => count($senders) > 1 ? "yes" : "no",
-                ],
-            ], 'friendship_request_sent');
-        }
+        //     SendEmailJob::dispatch([
+        //         'receiver' => $receiver,
+        //         'data' => [
+        //             'recipient' => $receiver->first_name,
+        //             'senders' => $senders,
+        //             'multiple' => count($senders) > 1 ? "yes" : "no",
+        //         ],
+        //     ], 'friendship_request_sent');
+        // }
     }
 }
