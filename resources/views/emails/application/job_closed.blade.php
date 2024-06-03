b3cdf528e37a402faedf7d7b140ba9959e24339a
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-06-04 00:11:09 +0100

    fix

--- resources/views/emails/application/job_closed.blade.php
+++ resources/views/emails/application/job_closed.blade.php
@@ -6,8 +6,8 @@
         style="padding: 10px; display: flex; flex-direction: column; row-gap: 10px; background-color: pink; justify-content: center; align-items: center">
         <div>Ignore this pink section, use the links given below to view different versions</div>
         <div style="display: flex; column-gap: 10px; justify-content: center; align-items: center">
-            <a class="btn btn-primary btn-link" href="?apply_type=Internal">Internal Job</a>
-            <a class="btn btn-primary btn-link" href="?apply_type=External">External Job</a>
+            <a class="btn btn-primary" href="?apply_type=Internal">Internal Job Closed Version</a>
+            <a class="btn btn-primary" href="?apply_type=External">External Job Closed Version</a>
         </div>
     </td>
 </tr>
