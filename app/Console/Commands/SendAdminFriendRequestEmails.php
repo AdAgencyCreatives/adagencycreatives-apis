e9e4a6378408bfaff42bcf3ddab8aaac4d0f8abc
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-06-11 04:26:44 +0100

    fix

--- app/Console/Commands/SendAdminFriendRequestEmails.php
+++ app/Console/Commands/SendAdminFriendRequestEmails.php
@@ -87,49 +87,51 @@ class SendAdminFriendRequestEmails extends Command
             }
         }
 
-        // $date_range = now()->format('Y-m-d');
+        $date_range = now()->format('Y-m-d');
 
-        // $friendRequests = FriendRequest::where(function ($q) use ($sender, $receiver) {
-        //     $q->where(function ($query) use ($sender, $receiver) {
-        //         $query->where('sender_id', $sender)->where('receiver_id', $receiver);
-        //     })->orWhere(function ($query) use ($sender, $receiver) {
-        //         $query->where('sender_id', $receiver)->where('receiver_id', $sender);
-        //     });
-        // })->where('status', 'pending')
-        //     ->whereDate('updated_at', '=', $date_range)
-        //     ->orderBy('receiver_id')->orderByDesc('updated_at')
-        //     ->get();
+        $friendRequests = FriendRequest::where(function ($q) use ($sender, $receiver) {
+            $q->where(function ($query) use ($sender, $receiver) {
+                $query->where('sender_id', $sender)->where('receiver_id', $receiver);
+            })->orWhere(function ($query) use ($sender, $receiver) {
+                $query->where('sender_id', $receiver)->where('receiver_id', $sender);
+            });
+        })->where('status', 'pending')
+            ->whereDate('updated_at', '=', $date_range)
+            ->orderBy('receiver_id')->orderByDesc('updated_at')
+            ->get();
 
-        // $bundle = [];
-        // $receivers = [];
+        $bundle = [];
+        $receivers = [];
 
-        // foreach ($friendRequests as $fr) {
-        //     $receiver = $fr->receiver;
-        //     $sender = $fr->sender;
+        foreach ($friendRequests as $fr) {
+            $receiver = $fr->receiver;
+            $sender = $fr->sender;
 
-        //     $sender->profile_picture = get_profile_picture($sender);
+            $sender->profile_picture = get_profile_picture($sender);
 
-        //     if (array_key_exists($receiver->id, $bundle)) {
-        //         $bundle[$receiver->id][count($bundle)] = $sender;
-        //     } else {
-        //         $bundle[$receiver->id] = array(0 => $sender);
-        //         $receivers[count($receivers)] = $receiver;
-        //     }
-        // }
+            if (array_key_exists($receiver->id, $bundle)) {
+                $bundle[$receiver->id][count($bundle)] = $sender;
+            } else {
+                $bundle[$receiver->id] = array(0 => $sender);
+                $receivers[count($receivers)] = $receiver;
+            }
+        }
 
-        // $this->info('Recipient Count: ');
+        $this->info('Recipient Count: ');
 
-        // foreach ($receivers as $receiver) {
-        //     $senders = $bundle[$receiver->id];
+        foreach ($receivers as $receiver) {
+            $senders = $bundle[$receiver->id];
 
-        //     SendEmailJob::dispatch([
-        //         'receiver' => $receiver,
-        //         'data' => [
-        //             'recipient' => $receiver->first_name,
-        //             'senders' => $senders,
-        //             'multiple' => count($senders) > 1 ? "yes" : "no",
-        //         ],
-        //     ], 'friendship_request_sent');
-        // }
+            // SendEmailJob::dispatch([
+            //     'receiver' => $receiver,
+            //     'data' => [
+            //         'recipient' => $receiver->first_name,
+            //         'senders' => $senders,
+            //         'multiple' => count($senders) > 1 ? "yes" : "no",
+            //     ],
+            // ], 'friendship_request_sent');
+
+            $this->info('Sent friend request email to: ' . $receiver->full_name);
+        }
     }
 }
