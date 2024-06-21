b1af619089587922953b9cffc3332d273df10b22
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-06-22 04:54:22 +0500

    fix

--- app/Http/Controllers/Api/V1/JobController.php
+++ app/Http/Controllers/Api/V1/JobController.php
@@ -59,8 +59,8 @@ class JobController extends Controller
                 'status',
                 AllowedFilter::trashed(),
             ])
-            ->defaultSort('-featured_at', '-updated_at', "-created_at")
-            ->allowedSorts('featured_at', 'updated_at', 'created_at');
+            ->allowedSorts('featured_at', 'updated_at', 'created_at')
+            ->defaultSort('-featured_at', '-updated_at', "-created_at");
 
         if ($industries !== null) {
             applyExperienceFilter($query, $industries, 'industry_experience', 'job_posts');
