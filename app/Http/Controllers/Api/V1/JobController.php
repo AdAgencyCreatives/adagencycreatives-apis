e186e57529254da31ef49d5a389da268f878006c
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-06-22 04:56:07 +0500

    fix

--- app/Http/Controllers/Api/V1/JobController.php
+++ app/Http/Controllers/Api/V1/JobController.php
@@ -59,8 +59,7 @@ class JobController extends Controller
                 'status',
                 AllowedFilter::trashed(),
             ])
-            ->allowedSorts('featured_at', 'updated_at', 'created_at')
-            ->defaultSort('-featured_at', '-updated_at', "-created_at");
+            ->allowedSorts('featured_at', 'updated_at', 'created_at');
 
         if ($industries !== null) {
             applyExperienceFilter($query, $industries, 'industry_experience', 'job_posts');
@@ -76,6 +75,8 @@ class JobController extends Controller
         if ($request->applications_count) {
             $jobs = $jobs->having('applications_count', '>=', $request->applications_count);
         }
+
+        $jobs->orderByDesc('featured_at')->orderByDesc('updated_at')->orderByDesc('created_at');
 
         $jobs = $jobs->with('applications', function ($query) {
             $query->orderBy('status', 'asc')->orderBy('id', 'desc');
