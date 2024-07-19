351f2427a5848f95e736f90887a3c3bfa2206a5e
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-07-19 19:14:43 +0500

    fix

--- resources/views/emails/application/removed.blade.php
+++ resources/views/emails/application/removed.blade.php
@@ -5,31 +5,39 @@
     <td>
         <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
             <tr>
-                <td style="padding: 0 0 30px; font-family: sans-serif; mso-height-rule: exactly; line-height: 14px; color: #000000; font-size: 14px; position: relative;" class="body_text_color body_text_size">
-                    <h1 style="background: #fff; text-align: center; padding: 30px; border-bottom: 2px solid #000;     text-transform: uppercase;">
+                <td style="padding: 0 0 30px; font-family: sans-serif; mso-height-rule: exactly; line-height: 14px; color: #000000; font-size: 14px; position: relative;"
+                    class="body_text_color body_text_size">
+                    <h1
+                        style="background: #fff; text-align: center; padding: 30px; border-bottom: 2px solid #000;     text-transform: uppercase;">
                         Status Update</h1>
-                    <div style="background:#fff; border-radius: 5px; max-width: 450px; margin: 0 auto; color:#000000; line-height:1.5 !important" class="content">
+                    <div style="background:#fff; border-radius: 5px; max-width: 450px; margin: 0 auto; color:#000000; line-height:1.5 !important"
+                        class="content">
 
                         <span style="font-weight: normal; font-size: 14px;" class="welcome">Hi
                             {{ $data['applicant'] }},</span>
 
-                        <p>The status of one of your job applications on <a href="{{ $data['APP_URL'] }}" target="_blank">{{ $data['APP_NAME'] }}</a> has changed.
+                        <p>The status of one of your job applications on <a href="{{ $data['APP_URL'] }}"
+                                target="_blank">{{ $data['APP_NAME'] }}</a> has changed.
                         </p>
 
-                        <p>@if (strlen($data['agency_profile']) > 0)
+                        <p>
+                            @if (strlen($data['agency_profile']) > 0)
                                 <a href="{{ $data['agency_profile'] }}" target="_blank">{{ $data['agency_name'] }}</a>
                             @else
                                 {{ $data['agency_name'] }}
                             @endif
                             reviewed your application and is not moving forward at this time for the
-                            <a href="{{ $data['job_url'] }}" target="_blank">{{ $data['job_title'] }}</a> opportunity. There are a variety of reason candidates for this, such as; chose
+                            <a href="{{ $data['job_url'] }}" target="_blank">{{ $data['job_title'] }}</a> opportunity.
+                            There are a variety of reason candidates for this, such as; chose
                             another candidate, an internal move was made, or the job was closed without a hire.
                         </p>
 
                         <p>As you know, our industry is alive and changing every moment. We encourage you
-                            to keep an eye on other opportunities. You might also try exploring the <a href="{{ $data['APP_URL'] }}/mentoring-resources" target="_blank">Resources</a>
+                            to keep an eye on other opportunities. You might also try exploring the <a
+                                href="{{ $data['APP_URL'] }}/mentoring-resources" target="_blank">Resources</a>
                             and <a href="{{ $data['APP_URL'] }}/publication-resources" target="_blank">Publications</a>
-                            sections on our home page, or connect with other creatives in The Lounge.
+                            sections on our home page, or connect with other creatives in <a
+                                href="{{ $data['FRONTEND_URL'] }}/community" target="_blank">The Lounge</a>.
                         </p>
 
                         <p>Trust that the right opportunity is looking for you. It's all about the timing.
