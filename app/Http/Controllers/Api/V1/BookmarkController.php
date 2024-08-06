9d24f7ccb7eaabd67e5267a36fc75142c2fc8736
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-08-06 05:13:38 +0500

    fix

--- app/Http/Controllers/Api/V1/BookmarkController.php
+++ app/Http/Controllers/Api/V1/BookmarkController.php
@@ -47,7 +47,7 @@ class BookmarkController extends Controller
             $filtered = [];
             foreach ($bookmarks as $bookmark) {
                 $user_full_name = $bookmark->user->first_name . " " . $bookmark->user->last_name;
-                if (strripos($user_full_name, $request->search) !== false) {
+                if (stripos($user_full_name, $request->search) !== false) {
                     $filtered[count($filtered)] = $bookmark;
                 }
             }
