afbdefea5c9daf2f901c2495ae38dc253a3690dc
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-08-06 05:30:48 +0500

    test

--- app/Http/Controllers/Api/V1/BookmarkController.php
+++ app/Http/Controllers/Api/V1/BookmarkController.php
@@ -46,7 +46,8 @@ class BookmarkController extends Controller
         if ($request->has('search')) {
             $filtered = [];
             foreach ($bookmarks as $bookmark) {
-                $user_full_name = $bookmark->user->first_name . " " . $bookmark->user->last_name;
+                $bookmark_user = $bookmark->bookmarkable->user;
+                $user_full_name = $bookmark_user->first_name . " " . $bookmark_user->last_name;
                 if (true || stripos($user_full_name, $request->search) !== false) {
                     // $filtered[count($filtered)] = $bookmark;
                     $filtered[count($filtered)] = $user_full_name;
