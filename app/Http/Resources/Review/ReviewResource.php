23af474608a7ae852191bb43271335c60a1216c1
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-08-31 12:27:45 +0500

    fix

--- app/Http/Resources/Review/ReviewResource.php
+++ app/Http/Resources/Review/ReviewResource.php
@@ -17,6 +17,7 @@ class ReviewResource extends JsonResource
             'user' => $user->full_name,
             'user_slug' => get_user_slug($user),
             'profile_picture' => get_profile_picture($user),
+            'profile_picture_base64' => $this->get_website_preview_base64($user),
             'target_id' => $this->target->uuid,
             'comment' => $this->comment,
             'rating' => $this->rating,
@@ -24,5 +25,15 @@ class ReviewResource extends JsonResource
             'created_at' => $this->created_at->format(config('global.datetime_format')),
             'updated_at' => $this->created_at->format(config('global.datetime_format')),
         ];
+    }
+
+    public function get_profile_picture_base64($user)
+    {
+        try {
+            $profile_picture = get_profile_picture($user);
+            return "data:image/jpeg;charset=utf-8;base64," . (strlen($profile_picture) > 0 ? base64_encode(file_get_contents($profile_picture)) : "");
+        } catch (\Exception $e) {
+        }
+        return "data:image/jpeg;charset=utf-8;base64,";
     }
 }
