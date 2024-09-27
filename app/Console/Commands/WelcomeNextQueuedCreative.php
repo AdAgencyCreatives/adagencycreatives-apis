40ce04cf4d99b88513dfbceabe1fce07e91151e3
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-09-28 00:59:23 +0500

    fix

--- app/Console/Commands/WelcomeNextQueuedCreative.php
+++ app/Console/Commands/WelcomeNextQueuedCreative.php
@@ -19,7 +19,7 @@ class WelcomeNextQueuedCreative extends Command {
     public function handle() {
 
         try {
-            $this->info( today()->toDateString() );
+            $this->info( 'Today: '. today()->toDateString() );
             $today_welcomed_at_creatives_count = Creative::where( 'is_welcomed', '=', 1 )->whereDate( 'welcomed_at', '=', today()->toDateString() )->count( 'welcomed_at' );
             $previous_welcome_queued_at_creatives_count = Creative::where( 'is_welcomed', '=', 0 )->whereNotNull( 'welcome_queued_at' )->count( 'welcome_queued_at' );
             $next_welcome_creative = null;
