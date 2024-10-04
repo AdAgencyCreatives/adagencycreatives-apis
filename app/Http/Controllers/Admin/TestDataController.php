4ef18df78e55f35ca1c5923c234fffa2aaff9d89
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-10-05 02:50:31 +0500

    fix

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -751,7 +751,7 @@ class TestDataController extends Controller
         $output[] = "Creatives: " . count($creatives);
 
         foreach ($creatives as $creative) {
-            $output[] = sprintf("Progress: %&nbsp;3d", $this->getCreativeProfileProgress($creative)) . ", Registered: " .  $creative?->user?->created_at?->format( config( 'global.datetime_format' ) ) . ", " . $creative?->user?->full_name;
+            $output[] = sprintf("Progress: % 3d", $this->getCreativeProfileProgress($creative)) . ", Registered: " .  $creative?->user?->created_at?->format( config( 'global.datetime_format' ) ) . ", " . $creative?->user?->full_name;
         }
 
         return implode("\n<br />", $output);
