a51696ae7b607c135ca915e31e44cdcdcadb0e18
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-10-07 20:38:03 +0500

    fix

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -799,9 +799,9 @@ class TestDataController extends Controller
                 $q->where('id', '=', $request->user_id);
             })->first();
         } else {
-            $creative = Creative::whereHas('user', function($q){
+            $creative = Creative::whereHas('user', function ($q) {
                 $q->whereNull('profile_completed_at')->orderBy('created_at');
-            }{})->take(1)->first();
+            })->take(1)->first();
         }
 
         if (!$creative) {
