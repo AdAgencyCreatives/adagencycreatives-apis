e90f7059d0f95718106bdafb6eef014c48013153
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-10-16 21:57:03 +0500

    fix

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -981,7 +981,10 @@ class TestDataController extends Controller
 
         $agency_users_without_job_posts = array_values(array_unique(array_diff($agency_user_ids, $job_user_ids)));
 
-        $agencies_without_job_posts = Agency::with('user')->whereIn('user_id', $agency_users_without_job_posts)->get();
-        return new AgencyCollection($agencies_without_job_posts);
+        $agencies_without_job_posts = Agency::withWhereHas('user', function ($q) {
+            $q->orderBy('created_at');
+        })->whereIn('user_id', $agency_users_without_job_posts)->get(["name"]);
+
+        return $agencies_without_job_posts;
     }
 }
