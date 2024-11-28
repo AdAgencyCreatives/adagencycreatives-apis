d7e001f8006c72a3c589d2e0869112bed5647808
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-11-29 02:54:21 +0500

    fix

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -26,44 +26,44 @@ class SearchController extends Controller
     {
         $search_items = [];
 
-        $categories = Category::orderBy('name')->select(DB::raw('"Category" as type'), 'name')->get()->toArray();
+        $categories = Category::orderBy('name')->select('name')->get()->toArray();
         if ($categories && count($categories) > 0) {
-            $search_items = array_merge($search_items, $categories);
+            $search_items['categories'] = $categories;
         }
 
-        $states = Location::whereNull('parent_id')->orderBy('name')->select(DB::raw('"State" as type'), 'name')->get()->toArray();
+        $states = Location::whereNull('parent_id')->orderBy('name')->select('name')->get()->toArray();
         if ($states && count($states) > 0) {
-            $search_items = array_merge($search_items, $states);
+            $search_items['states'] = $states;
         }
 
-        $cities = Location::whereNotNull('parent_id')->orderBy('name')->select(DB::raw('"City" as type'), 'name')->get()->toArray();
+        $cities = Location::whereNotNull('parent_id')->orderBy('name')->select('name')->get()->toArray();
         if ($cities && count($cities) > 0) {
-            $search_items = array_merge($search_items, $cities);
+            $search_items['cities'] = $cities;
         }
 
-        $employment_types = EmploymentTypes::orderBy('name')->select(DB::raw('"Employment Type" as type'), 'name')->get()->toArray();
+        $employment_types = EmploymentTypes::orderBy('name')->select('name')->get()->toArray();
         if ($employment_types && count($employment_types) > 0) {
-            $search_items = array_merge($search_items, $employment_types);
+            $search_items['employment_types'] = $employment_types;
         }
 
-        $years_of_experience = YearsOfExperience::orderBy('name')->select(DB::raw('"Years of Experience" as type'), 'name')->get()->toArray();
+        $years_of_experience = YearsOfExperience::orderBy('name')->select('name')->get()->toArray();
         if ($years_of_experience && count($years_of_experience) > 0) {
-            $search_items = array_merge($search_items, $years_of_experience);
+            $search_items['years_of_experience'] = $years_of_experience;
         }
 
-        $media_experiences = Media::orderBy('name')->select(DB::raw('"Media Experience" as type'), 'name')->get()->toArray();
+        $media_experiences = Media::orderBy('name')->select('name')->get()->toArray();
         if ($media_experiences && count($media_experiences) > 0) {
-            $search_items = array_merge($search_items, $media_experiences);
+            $search_items['media_experiences'] = $media_experiences;
         }
 
-        $Strengths = Strength::orderBy('name')->select(DB::raw('"Strength" as type'), 'name')->get()->toArray();
-        if ($Strengths && count($Strengths) > 0) {
-            $search_items = array_merge($search_items, $Strengths);
+        $strengths = Strength::orderBy('name')->select('name')->get()->toArray();
+        if ($strengths && count($strengths) > 0) {
+            $search_items['Strengths'] = $strengths;
         }
 
-        $industry_experiences = Industry::orderBy('name')->select(DB::raw('"Industry Experience" as type'), 'name')->get()->toArray();
+        $industry_experiences = Industry::orderBy('name')->select('name')->get()->toArray();
         if ($industry_experiences && count($industry_experiences) > 0) {
-            $search_items = array_merge($search_items, $industry_experiences);
+            $search_items['industry_experiences'] = $industry_experiences;
         }
 
         return $search_items;
