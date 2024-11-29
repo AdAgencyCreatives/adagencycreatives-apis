0dcfcf744097451e0e2e06be782684d8eb1b3916
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-11-30 04:11:41 +0500

    fix

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -31,67 +31,67 @@ class SearchController extends Controller
         $categories = Category::select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($categories && count($categories) > 0) {
-            $search_items['categories'] = $categories;
+            $search_items['Category'] = $categories;
         }
 
         $states = Location::select('name')->distinct()->whereNull('parent_id')->orderBy('name')->get()->toArray();
 
         if ($states && count($states) > 0) {
-            $search_items['states'] = $states;
+            $search_items['State'] = $states;
         }
 
         $cities = Location::select('name')->distinct()->whereNotNull('parent_id')->orderBy('name')->get()->toArray();
         if ($cities && count($cities) > 0) {
-            $search_items['cities'] = $cities;
+            $search_items['City'] = $cities;
         }
 
         $employment_types = EmploymentTypes::select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($employment_types && count($employment_types) > 0) {
-            $search_items['employment_types'] = $employment_types;
+            $search_items['Employment Type'] = $employment_types;
         }
 
         $years_of_experience = YearsOfExperience::select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($years_of_experience && count($years_of_experience) > 0) {
-            $search_items['years_of_experience'] = $years_of_experience;
+            $search_items['Years of Experience'] = $years_of_experience;
         }
 
         $media_experiences = Media::select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($media_experiences && count($media_experiences) > 0) {
-            $search_items['media_experiences'] = $media_experiences;
+            $search_items['Media Experience'] = $media_experiences;
         }
 
         // limited by the client to load only BICOP and Bilingual
         $strengths = Strength::whereIn('name', ["BICOP", "Bilingual"])->select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($strengths && count($strengths) > 0) {
-            $search_items['strengths'] = $strengths;
+            $search_items['Strength'] = $strengths;
         }
 
         $industry_experiences = Industry::select('name')->distinct()->orderBy('name')->get()->toArray();
 
         if ($industry_experiences && count($industry_experiences) > 0) {
-            $search_items['industry_experiences'] = $industry_experiences;
+            $search_items['Industry Experience'] = $industry_experiences;
         }
 
         $education_colleges = Education::select('college as name')->distinct()->orderBy('college')->get()->toArray();
 
         if ($education_colleges && count($education_colleges) > 0) {
-            $search_items['education_colleges'] = $education_colleges;
+            $search_items['Education College'] = $education_colleges;
         }
 
         $education_degrees = Education::select('degree as name')->distinct()->orderBy('degree')->get()->toArray();
 
         if ($education_degrees && count($education_degrees) > 0) {
-            $search_items['education_degrees'] = $education_degrees;
+            $search_items['Education Degree'] = $education_degrees;
         }
 
         $experience_companies = Experience::select('company as name')->distinct()->orderBy('company')->get()->toArray();
 
         if ($experience_companies && count($experience_companies) > 0) {
-            $search_items['experience_companies'] = $experience_companies;
+            $search_items['Experience Company'] = $experience_companies;
         }
 
         return $search_items;
