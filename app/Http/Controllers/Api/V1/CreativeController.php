6efd0f7eafa273baf641427b1e88f55872c2b24f
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2024-12-16 23:09:24 +0500

    fix

--- app/Http/Controllers/Api/V1/CreativeController.php
+++ app/Http/Controllers/Api/V1/CreativeController.php
@@ -116,73 +116,6 @@ class CreativeController extends Controller
         return new LoggedinCreativeCollection($creatives);
     }
 
-    // {
-    //     $role = $request?->role ?? 'agency';
-
-    //     $agency_user_id = $request?->user()?->id;
-    //     $agency_user_applicants = [];
-    //     if (isset($agency_user_id)) {
-    //         $agency_user_applicants = array_unique(Application::whereHas('job', function ($query) use ($agency_user_id) {
-    //             $query->where('user_id', $agency_user_id);
-    //         })->pluck('user_id')->toArray());
-    //     }
-
-    //     /**
-    //      * SEARCH CRITERIA
-    //      *
-    //      * 1. Exact 1+2+3: Art Director in Chicago with CPG experience
-    //      * 2. Exact 1+2: Art Director in Chicago with no CPG experience
-    //      * 3. Contains 1 + Exact 2: All creatives with "contains" Art Director in their title (because sometimes we can go up and down a level for the right person) in Chicago. So this search would provide me Jr Art Directors and Senior Art Directors in Chicago. So Contains part of the first search, but the exact second
-    //      * 4. Exact 1:  All creatives with the exact title "Art Director" in any location even in Canada (some jobs will allow remote if they are a perfect fit otherwise in emergencies)
-    //      */
-
-    //     // Split the search terms into an array
-    //     $searchTerms = explode(',', $request->search);
-    //     $searchTermsLevel2 = explode(',', $request->search_level2 ?? "");
-
-    //     $combinedCreativeIds = [];
-    //     if (count($searchTerms) === 1) {
-    //         $combinedCreativeIds = $this->process_single_term_search($searchTerms[0], $role);
-    //     } else {
-    //         $combinedCreativeIds = $this->process_three_terms_search($searchTerms, $role);
-    //     }
-
-    //     $combinedCreativeIdsLevel2 = [];
-    //     if (count($searchTermsLevel2) > 0) {
-    //         if (count($searchTermsLevel2) === 1) {
-    //             $combinedCreativeIdsLevel2 = $this->process_single_term_search($searchTermsLevel2[0], $role);
-    //         } else {
-    //             $combinedCreativeIdsLevel2 = $this->process_three_terms_search($searchTermsLevel2, $role);
-    //         }
-    //     }
-
-    //     $combinedCreativeIds = Arr::flatten($combinedCreativeIds);
-    //     // Combine and deduplicate the IDs while preserving the order
-    //     $combinedCreativeIds = array_values(array_unique($combinedCreativeIds, SORT_NUMERIC));
-
-    //     $combinedCreativeIds = array_values(array_unique(array_intersect($combinedCreativeIds, $combinedCreativeIdsLevel2)));
-
-    //     $rawOrder = 'FIELD(id, ' . implode(',', $combinedCreativeIds) . ')';
-
-    //     // Retrieve creative records from the database and order them based on the calculated order
-    //     $creatives = Creative::with('category')
-    //         ->whereIn('id', $combinedCreativeIds)
-    //         ->whereHas('user', function ($query) use ($agency_user_applicants) {
-    //             $query->where('status', 1)
-    //                 ->where(function ($q) use ($agency_user_applicants) {
-    //                     $q->where('is_visible', 1)
-    //                         ->orWhere(function ($q1) use ($agency_user_applicants) {
-    //                             $q1->where('is_visible', 0)
-    //                                 ->whereIn('user_id', $agency_user_applicants);
-    //                         });
-    //                 });
-    //         })
-    //         ->paginate($request->per_page ?? config('global.request.pagination_limit'))
-    //         ->withQueryString();
-
-    //     return new LoggedinCreativeCollection($creatives);
-    // }
-
     public function search3(Request $request)
     {
         $role = $request?->role ?? 'agency';
@@ -318,8 +251,8 @@ class CreativeController extends Controller
         for ($i = 0; $i < count($terms); $i++) {
             $term = trim($terms[$i]);
 
-            $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "CONCAT(ur.first_name, ' ', ur.last_name) LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-            $sql .= " OR CONCAT(ur.last_name, ' ', ur.first_name) LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
+            $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(CONCAT(ur.first_name, ' ', ur.last_name) LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "')" . "\n";
+            $sql .= " OR (CONCAT(ur.last_name, ' ', ur.first_name) LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "')" . "\n";
         }
 
         $sql .= 'UNION DISTINCT' . "\n";
@@ -351,7 +284,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ind.name LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ind.name LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(ind.name LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -361,7 +294,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "md.name LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "md.name LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(md.name LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -371,7 +304,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "st.name LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "st.name LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(st.name LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -381,7 +314,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "cr.employment_type LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "cr.employment_type LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(cr.employment_type LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -390,7 +323,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "cr.years_of_experience LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "cr.years_of_experience LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(cr.years_of_experience LIKE '" . trim($term) . "')" . "\n";
             }
 
             $workplace_preferences = [
@@ -417,7 +350,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ed.degree LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ed.degree LIKE '" . trim($term) . "')" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(ed.degree LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -426,7 +359,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ed.college LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ed.college LIKE '" . trim($term) . "'" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(ed.college LIKE '" . trim($term) . "')" . "\n";
             }
 
             $sql .= 'UNION DISTINCT' . "\n";
@@ -435,7 +368,7 @@ class CreativeController extends Controller
             for ($i = 0; $i < count($terms); $i++) {
                 $term = $terms[$i];
                 // $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ex.company LIKE '" . $wildCardStart . '' . trim($term) . '' . $wildCardEnd . "'" . "\n";
-                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "ex.company LIKE '" . trim($term) . "'" . "\n";
+                $sql .= ($i == 0 ? ' WHERE ' : ' OR ') . "(ex.company LIKE '" . trim($term) . "')" . "\n";
             }
         }
 
