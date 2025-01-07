c1966064c911867e3154d345ee040f9ab240ffc5
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-01-08 01:38:33 +0500

    fix

--- resources/views/emails/includes/jobboard_footer.blade.php
+++ resources/views/emails/includes/jobboard_footer.blade.php
@@ -1,7 +1,8 @@
 <p>
     Thanks,<br />
     Member Support<br />
-    <img width="200" src="https://ad-agency-creatives.s3.amazonaws.com/agency_logo/ad-agency-creatives-logo.png"
+    <img width="200"
+        src="https://ad-agency-creatives.s3.us-east-1.amazonaws.com/agency_logo/ad-agency-creatives-logo-white-black.png"
         alt="" />
 </p>
 <p style="text-align: center; margin-bottom: 0;">Gather. Inspire. Do Cool $#*t!</p>
