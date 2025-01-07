0d5f0689913289328f38b1f4d1a349f72edcd432
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-01-08 00:21:21 +0500

    fix

--- resources/views/emails/job/custom-package-request-rejected.blade.php
+++ resources/views/emails/job/custom-package-request-rejected.blade.php
@@ -176,7 +176,6 @@
                                                     <img class="footer-logo"
                                                         style="width: 120px; position: absolute; right: 15px; bottom: 15px;"
                                                         src="https://ad-agency-creatives.s3.amazonaws.com/agency_logo/cropped-Ad-Agency-Creatives-Logo.png">
-
                                                 </div>
                                             </td>
                                         </tr>
@@ -191,18 +190,22 @@
                         <!-- Email Footer : BEGIN -->
                         <br>
                         <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="left"
-                            width="100%" style="max-width: 600px; border-radius: 5px;" bgcolor="#0a0909"
+                            width="100%" style="max-width: 600px; border-radius: 5px;" bgcolor="#000000"
                             class="footer_bg">
                             <tr>
-                                <td style="padding: 20px; width: 100%; font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height: 19px; text-align: left; color: #ffffff; word-break: break-all;"
+                                <td style="padding: 15px; width: 100%; font-size: 14px; font-family: sans-serif; mso-height-rule: exactly; line-height: 14px; text-align: left; color: #ffffff; word-break: break-all;"
                                     class="footer_text_color footer_text_size">
                                     <div style="text-align:center;">
-                                        <img width="40%"
-                                            src="https://adagencycreatives.com/wp-content/uploads/2022/04/AAC-LOGO-500-×-100-px-1.png"
-                                            alt="" />
-                                        <a style="display:inline-block;width:100%;"
-                                            href="mailto:info@agencycreatives.com"
-                                            style="text-decoration: underline;">info@agencycreatives.com</a>
+                                        <div>© {{ date('Y') }} Ad Agency Creatives. All Rights Reserved.</div>
+                                        <div style="display:inline-block; margin-top:5px;" id="footer">
+                                            <a style="color: #ffffff; margin-right: 15px;"
+                                                href="https://adagencycreatives.com/privacy-policy/">Privacy Policy</a>
+                                            <a style="color: #ffffff; margin-right: 15px;"
+                                                href="https://adagencycreatives.com/terms-and-conditions/">User
+                                                Agreement</a>
+                                            <a style="color: #ffffff;"
+                                                href="https://adagencycreatives.com/contact">Contact Us</a>
+                                        </div>
                                     </div>
                                 </td>
                             </tr>
