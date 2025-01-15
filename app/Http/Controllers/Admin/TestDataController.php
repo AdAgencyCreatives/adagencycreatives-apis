2d48afef166fb9e4ee8237410828a594ce1c5ce8
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-01-15 18:58:00 +0500

    fix

--- app/Http/Controllers/Admin/TestDataController.php
+++ app/Http/Controllers/Admin/TestDataController.php
@@ -1226,7 +1226,7 @@ class TestDataController extends Controller
 
     public function testRegenThumb(Request $request)
     {
-        $user = User::where('regen_thumb', '<>', 1)->get();
+        $user = User::where('regen_thumb', '<>', 1)->orWhereNull('regen_thumb')->limit(1)->first();
         return json_encode($user);
     }
 }
