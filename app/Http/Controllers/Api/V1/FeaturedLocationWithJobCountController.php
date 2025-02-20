cb3fcd36a9835a7bb886aacc6b780ffaa6b54dfa
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-02-20 21:10:29 +0500

    fix

--- app/Http/Controllers/Api/V1/FeaturedLocationWithJobCountController.php
+++ app/Http/Controllers/Api/V1/FeaturedLocationWithJobCountController.php
@@ -17,14 +17,13 @@ class FeaturedLocationWithJobCountController extends Controller
 {
     public function index(Request $request)
     {
-        // $query = Location::select('locations.*', \DB::raw('COUNT(job_posts.id) as job_count'))
-        //     ->leftJoin('job_posts', function ($join) {
-        //         $join->on('locations.id', '=', 'job_posts.city_id');
-        //     })
-        //     ->groupBy('locations.id', 'locations.uuid', 'locations.name', 'locations.slug', 'locations.parent_id', 'locations.preview_link', 'locations.is_featured', 'locations.created_at', 'locations.updated_at');
+        $query = Location::select('locations.*', DB::raw('(SELECT COUNT(job_posts.id) FROM job_posts WHERE job_posts.city_id = locations.id AND job_posts.status = 1) as job_count'));
+        // ->leftJoin('job_posts', function ($join) {
+        //     $join->on('locations.id', '=', 'job_posts.city_id');
+        // })
+        // ->groupBy('locations.id', 'locations.uuid', 'locations.name', 'locations.slug', 'locations.parent_id', 'locations.preview_link', 'locations.is_featured', 'locations.created_at', 'locations.updated_at');
 
-
-        $query = Location::selectRaw("SELECT T.* FROM (SELECT locations.*, (SELECT COUNT(*) FROM job_posts WHERE job_posts.city_id = locations.id AND job_posts.status = 1) as job_count FROM locations) T ORDER BY T.sort_order ASC, T.job_count DESC;");
+        // $query = Location::selectRaw("SELECT T.* FROM (SELECT locations.*, (SELECT COUNT(*) FROM job_posts WHERE job_posts.city_id = locations.id AND job_posts.status = 1) as job_count FROM locations) T ORDER BY T.sort_order ASC, T.job_count DESC;");
 
         $perPage = $request->per_page ?? config('global.request.pagination_limit');
 
