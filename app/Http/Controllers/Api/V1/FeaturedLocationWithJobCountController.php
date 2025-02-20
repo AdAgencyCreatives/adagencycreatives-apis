16de097b3ad1442adbcbb35fd148d8e1a815fe7e
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-02-20 13:50:42 +0500

    fix

--- app/Http/Controllers/Api/V1/FeaturedLocationWithJobCountController.php
+++ app/Http/Controllers/Api/V1/FeaturedLocationWithJobCountController.php
@@ -18,8 +18,7 @@ class FeaturedLocationWithJobCountController extends Controller
     {
         $query = Location::select('locations.*', \DB::raw('COUNT(job_posts.id) as job_count'))
             ->leftJoin('job_posts', function ($join) {
-                $join->on('locations.id', '=', 'job_posts.state_id')
-                    ->orOn('locations.id', '=', 'job_posts.city_id');
+                $join->on('locations.id', '=', 'job_posts.city_id');
             })
             ->where('job_posts.status', '=', 1)
             ->groupBy('locations.id', 'locations.uuid', 'locations.name', 'locations.slug', 'locations.parent_id', 'locations.preview_link', 'locations.is_featured', 'locations.created_at', 'locations.updated_at');
