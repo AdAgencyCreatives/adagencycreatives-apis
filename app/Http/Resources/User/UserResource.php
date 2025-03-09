22e1bd8dd6b3b234f7eba21e187358d56c3aad91
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-03-09 15:30:26 +0500

    fix

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
+            'name' => $this->first_name . (!empty($this->last_name) ? ' ' . $this->last_name : ''),
             'username' => $this->username,
             'email' => $this->email,
             'role' => $this->role,
