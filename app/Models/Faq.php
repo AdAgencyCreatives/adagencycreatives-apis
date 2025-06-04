21eb31ad2bd66a13cd9cb0ad86456ecf335be475
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-04 15:53:26 +0500

    fixes

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Faq extends Model
 {
