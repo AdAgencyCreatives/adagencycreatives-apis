11afbf7fce9aa02099aa4e0350a5ebfc663f3365
ali-asghar-dev <ali.asghar@aiivatech.com>
2025-09-19 20:54:13 +0500

    meta controller updation

---
+++ app/Http/Controllers/Api/V1/MetaController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Agency;
+use App\Models\Creative;
+use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class MetaController extends Controller
+{
+    /**
+     * Get meta data for SEO purposes.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getMetaData(Request $request): JsonResponse
+    {
+        $type = $request->get('type');
+        $slug = $request->get('slug');
+
+        if (!$type || !$slug) {
+            return response()->json(['error' => 'Missing required parameters.'], 400);
+        }
+
+        $data = null;
+
+        switch ($type) {
+            case 'creative':
+                $creative = Creative::where('slug', $slug)->first();
+                if ($creative) {
+                    $data = [
+                        'name' => $creative->user->first_name . ' ' . $creative->user->last_name,
+                        'category' => $creative->category?->name,
+                        'about' => $creative->about,
+                    ];
+                }
+                break;
+
+            case 'agency':
+                $agency = Agency::where('slug', $slug)->first();
+                if ($agency) {
+                    $data = [
+                        'name' => $agency->name,
+                        'company' => $agency->name,
+                        'about' => $agency->about,
+                    ];
+                }
+                break;
+
+            case 'job':
+                $job = Job::where('slug', $slug)->first();
+                if ($job) {
+                    $data = [
+                        'title' => $job->title,
+                        'about' => $job->description,
+                    ];
+                }
+                break;
+
+            default:
+                return response()->json(['error' => 'Invalid type.'], 400);
+        }
+
+        if (!$data) {
+            return response()->json(['error' => 'Record not found.'], 404);
+        }
+
+        return response()->json($data);
+    }
+}
